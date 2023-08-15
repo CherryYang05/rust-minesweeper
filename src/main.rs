108523420f34aea0e9a6a5e7e6ca958929f9b0c8
CherryYang05 <754449629@qq.com>
2023-08-15 19:34:25 +0800

    实现鼠标左右键功能，左键：显示当前Tile内容；右键：实现插旗和取消功能。安全块的扩展尚未实现

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use map::Matrix;
-
 mod event;
 mod map;
 mod status;
 
 use map::BACKGROUND_COLOR;
+use map::Matrix;
 use map::TILE_WIDTH;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
 
 pub static mut DEBUG: bool = true;
+
 fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
@@ -36,13 +37,14 @@ fn main() {
     let mut map = Matrix::new(600 / TILE_WIDTH as usize, 800 / TILE_WIDTH as usize);
 
     map.draw_map(&mut canvas).unwrap();
-    map.generate_mine(&mut canvas, 15);
-    map.generate_num(&mut canvas);
+    map.generate_mine(10);
+    map.generate_num();
 
     // 更新屏幕
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
+
     // let mut event = Events::new(event_pump);
 
     'running: loop {
@@ -51,6 +53,8 @@ fn main() {
         //     break 'running
         // }
 
+        let mouse_state = event_pump.mouse_state();
+
         for event in event_pump.poll_iter() {
             match event {
                 // 按下窗口右上角关闭键结束整个程序
@@ -58,7 +62,9 @@ fn main() {
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => {
+                    break 'running
+                },
 
                 // 按下键盘上方向键设置 Debug 模式
                 Event::KeyDown {
@@ -71,10 +77,35 @@ fn main() {
                     } else {
                         map.draw_tiles(&mut canvas);
                     }
-                    canvas.present();
                 },
+
+                // 鼠标左键按下显示 Tile
+                Event::MouseButtonDown {
+                    mouse_btn: MouseButton::Left,
+                    ..
+                } => {
+                    map.show_tile(&mut canvas, &mouse_state);
+                },
+
+                // 鼠标右键按下插小旗子
+                Event::MouseButtonDown {
+                    mouse_btn: MouseButton::Right,
+                    ..
+                } => {
+                    map.set_flag(&mut canvas, &mouse_state);
+                },
+
                 _ => {}
             }
         }
+        // map.draw_map(&mut canvas).unwrap();
+
+        
+        // if mouse_state.right() {
+            
+        // }
+
+        // map::set_tile_highlight(&mut canvas, mouse_state);
+        canvas.present();
     }
 }
--- src/status.rs
+++ src/status.rs
@@ -1,11 +1,38 @@
 /// 处理地图中的一系列状态事件
 /// 例如 Debug 模式，游戏开始和结束状态
 ///
-use crate::map;
 
-pub fn debug_switch(map: &map::Matrix, debug: bool) {
-    if debug == true {
-        
-    } else {
+use bitflags::bitflags;
+
+bitflags! {
+    struct f: u8 {
+        const A = 0b1;
+        const B = 0b10;
+        const AB = Self::A.bits();
+        const C = Self::AB.bits();
     }
+}
+
+
+fn get() {
+    let x = f{bits: 1};
+    
+}
+
+use serde::Serialize;
+use serde::Deserialize;
+
+#[derive(Deserialize, Serialize)]
+struct Animal {
+    name: String,
+    age: u8
+}
+
+pub fn ser() {
+    let animal = Animal {
+        name: "cat".to_owned(),
+        age: 3
+    };
+    let json = serde_json::to_string(&animal).unwrap();
+    println!("{}", json);
 }
