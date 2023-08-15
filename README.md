3a8e16f1ad4b614e4e13e382a99c107f26454fe3
CherryYang05 <754449629@qq.com>
2023-08-15 19:34:25 +0800

    实现鼠标左右键功能，左键：显示当前Tile内容；右键：实现插旗和取消功能。安全块的扩展尚未实现

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## minesweeper
 
-### Reference
+### reference
 
 [SDL库安装](https://www.cnblogs.com/kileyi/p/11375216.html)
+
 [基于C++和SDL的软渲染从0搭建学习](https://zhuanlan.zhihu.com/p/428302382)
--- src/map.rs
+++ src/map.rs
@@ -5,15 +5,19 @@
 use std::path::Path;
 
 use rand::Rng;
+use sdl2::mouse::MouseState;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
-use sdl2::surface::Surface;
+use sdl2::surface::{Surface, SurfaceRef};
+use sdl2::sys::{SDL_GetMouseState, SDL_MapRGB, SDL_SetColorKey};
 use sdl2::video::Window;
 
 pub static TILE_WIDTH: u32 = 40; // 每个小格的宽度
 pub static BACKGROUND_COLOR: Color = Color::RGB(198, 198, 198);
 pub static BOARD_COLOR: Color = Color::RGB(128, 128, 128);
+pub static SAFE_COLOR: Color = Color::RGB(165, 165, 165);
+pub static HOVER_COLOR: Color = Color::RGB(220, 220, 220);
 
 pub struct Matrix {
     n: usize,
@@ -33,16 +37,23 @@ pub enum Tile {
     SAFE,    // 没有数字的 Tile
 }
 
-// /// 为自定义结构体 Tile 实现 PartialEq trait 以实现结构体之间比较大小
-// impl PartialEq for Tile {
-//     fn eq(&self, other: &Self) -> bool {
+/// 为自定义结构体 Tile 实现 PartialEq trait 以实现结构体之间比较大小
+impl PartialEq for Tile {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::FLAG, Self::FLAG) => true,
+            (Self::MINE, Self::MINE) => true,
+            (Self::UNKNOWN, Self::UNKNOWN) => true,
+            (Self::NUM(_), Self::NUM(_)) => true,
+            (Self::SAFE, Self::SAFE) => true,
+            _ => false,
+        }
+    }
 
-//     }
+    // fn ne(&self, other: &Self) -> bool {
 
-//     fn ne(&self, other: &Self) -> bool {
-
-//     }
-// }
+    // }
+}
 
 impl Matrix {
     // n * m 矩阵
@@ -55,14 +66,14 @@ impl Matrix {
     pub fn draw_map(&self, canvas: &mut Canvas<Window>) -> Result<(), String> {
         for mx in 0..self.n {
             for my in 0..self.m {
-                draw_one_tile(canvas, mx, my)?;
+                draw_one_tile(canvas, BACKGROUND_COLOR, mx, my)?;
             }
         }
         Ok(())
     }
 
     /// 随机生成地雷
-    pub fn generate_mine(&mut self, canvas: &mut Canvas<Window>, mut mine_num: usize) {
+    pub fn generate_mine(&mut self, mut mine_num: usize) {
         let mut rng = rand::thread_rng();
         while mine_num > 0 {
             let r_mx = rng.gen_range(0..self.n);
@@ -77,7 +88,7 @@ impl Matrix {
     }
 
     /// 生成地雷周围的数字
-    pub fn generate_num(&mut self, canvas: &mut Canvas<Window>) {
+    pub fn generate_num(&mut self) {
         // let mut mine_num: Vec<Vec<u8>> = vec![vec![0; self.m]; self.n];
         for mx in 0..self.n {
             for my in 0..self.m {
@@ -105,19 +116,48 @@ impl Matrix {
         }
     }
 
+    // 给所有 Tile 设置 img 或背景
     pub fn draw_tiles(&self, canvas: &mut Canvas<Window>) {
         for mx in 0..self.n {
             for my in 0..self.m {
                 let t = &self.data[mx][my];
-                set_tile_from_img(canvas, &t, mx, my);
+                set_tile_from_img(canvas, t, mx, my);
             }
         }
+    }
+
+    // 插小旗子，再次点击取消插旗
+    pub fn set_flag(&mut self, canvas: &mut Canvas<Window>, mouse_state: &MouseState) {
+        let tile = mouse_key_in_which_tile(mouse_state.x(), mouse_state.y());
+        // let tile_state = self.data[tile.0][tile.1];
+        // 如果当前 Tile 不是旗子，则插上旗子，否则取消旗子
+        if self.data[tile.0][tile.1] != Tile::FLAG {
+            self.data[tile.0][tile.1] = Tile::FLAG;
+            set_tile_from_img(canvas, &Tile::FLAG, tile.0, tile.1);
+        } else {
+            self.data[tile.0][tile.1] = Tile::SAFE;
+            set_tile_from_img(canvas, &Tile::SAFE, tile.0, tile.1)
+        }
+    }
+
+    // 左键点击显示 Tile，并显示连续的安全区域
+    pub fn show_tile(&mut self, canvas: &mut Canvas<Window>, mouse_state: &MouseState) {
+        let tile = mouse_key_in_which_tile(mouse_state.x(), mouse_state.y());
+        set_tile_from_img(canvas, &self.data[tile.0][tile.1], tile.0, tile.1);
     }
 }
 
 /// 渲染单独某一块 Tile 的边框和底色
-fn draw_one_tile(canvas: &mut Canvas<Window>, mx: usize, my: usize) -> Result<(), String> {
-    canvas.set_draw_color(BACKGROUND_COLOR);
+fn draw_one_tile(canvas: &mut Canvas<Window>, color: Color, mx: usize, my: usize) -> Result<(), String> {
+    // let tile: (u32, u32);
+    // let mouse_state = unsafe { SDL_GetMouseState(tile.0, tile.1) };
+    // let tile = mouse_key_in_which_tile(mouse_state.x(), mouse_state.y());
+    // if let (mx, my) = tile {
+    //     canvas.set_draw_color(HOVER_COLOR);
+    // } else {
+    // }
+    canvas.set_draw_color(color);
+    // canvas.fill_rect(get_tile_rect(tile.0, tile.1)).unwrap();
     canvas.fill_rect(get_tile_rect(mx, my))?;
     canvas.set_draw_color(BOARD_COLOR);
     canvas.draw_rect(get_tile_rect(mx, my))?;
@@ -129,11 +169,14 @@ fn set_tile_from_img(canvas: &mut Canvas<Window>, t: &Tile, mx: usize, my: usize
     let texture_creator = canvas.texture_creator();
     match t {
         Tile::MINE => {
-            let surface = Surface::load_bmp(Path::new("./assets/mine.bmp")).unwrap();
+            // SDL_SetColorKey(surface, flag, key)
+            let mut surface = Surface::load_bmp(Path::new("./assets/mine.bmp")).unwrap();
+            // SurfaceRef::set_color_key(&mut surface, true, BACKGROUND_COLOR).unwrap();
+            // SDL_MapRGB(format, r, g, b)
+            // let texture = texture_creator.
             let texture = texture_creator
                 .create_texture_from_surface(surface)
                 .unwrap();
-
             canvas.copy(&texture, None, get_tile_rect(mx, my)).unwrap();
         }
         Tile::NUM(num) => {
@@ -143,9 +186,18 @@ fn set_tile_from_img(canvas: &mut Canvas<Window>, t: &Tile, mx: usize, my: usize
                 let texture = texture_creator
                     .create_texture_from_surface(surface)
                     .unwrap();
-
                 canvas.copy(&texture, None, get_tile_rect(mx, my)).unwrap();
             }
+        }
+        Tile::FLAG => {
+            let surface = Surface::load_bmp(Path::new("./assets/flag.bmp")).unwrap();
+            let texture = texture_creator
+                .create_texture_from_surface(surface)
+                .unwrap();
+            canvas.copy(&texture, None, get_tile_rect(mx, my)).unwrap();
+        }
+        Tile::SAFE => {
+            draw_one_tile(canvas, SAFE_COLOR, mx, my).unwrap();
         }
         _ => (),
     }
@@ -162,5 +214,21 @@ fn get_tile_rect(x: usize, y: usize) -> Rect {
         (TILE_WIDTH * x as u32) as i32,
         TILE_WIDTH,
         TILE_WIDTH,
+    )
+}
+
+pub fn set_tile_highlight(canvas: &mut Canvas<Window>, mouse_state: MouseState) {
+    let tile = mouse_key_in_which_tile(mouse_state.x(), mouse_state.y());
+    canvas.set_draw_color(HOVER_COLOR);
+    canvas.fill_rect(get_tile_rect(tile.0, tile.1)).unwrap();
+    canvas.set_draw_color(BOARD_COLOR);
+    canvas.draw_rect(get_tile_rect(tile.0, tile.1)).unwrap();
+}
+
+/// 检测当前鼠标位于哪一块 Tile 上
+fn mouse_key_in_which_tile(x: i32, y: i32) -> (usize, usize) {
+    (
+        y as usize / TILE_WIDTH as usize,
+        x as usize / TILE_WIDTH as usize,
     )
 }
